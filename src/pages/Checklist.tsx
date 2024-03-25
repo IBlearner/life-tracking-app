fbc139c16e2068cbb45f311f70183270ce72ea42
kienvi <kienvi96@hotmail.com>
2024-03-25 21:20:21 +1000

    Adding basic input function for Checklist.

--- src/pages/Checklist.tsx
+++ src/pages/Checklist.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState, MouseEvent } from "react";
 import "./Checklist.scss";
 import { CiViewList } from "react-icons/ci";
 
@@ -11,28 +11,72 @@ interface IChecklistItem {
 
 export const Checklist = () => {
 	const [data, setData] = useState<IChecklistItem[]>([]);
+	const [itemInput, setItemInput] = useState<string>("");
+
 	const getInputField = () => {
 		return (
-			<>
-				<input />
+			<form>
+				<input onChange={(e) => onInputFieldChange(e)} value={itemInput} required={true} />
 				<button
-					onClick={() =>
-						addItemToList({
-							name: "dfnsfd",
-							completed: false,
-							createdDate: "dfsf-dfs-ds"
-						})
-					}
+					onClick={() => {
+						if (itemInput.trim() !== "") {
+							addItemToList();
+						}
+					}}
+					type="submit"
+					disabled={itemInput.trim() === ""}
 				>
-					go
+					Add
 				</button>
-			</>
+			</form>
 		);
 	};
 
-	const addItemToList = (newItem: IChecklistItem) => {
+	const onInputFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setItemInput(e.target.value);
+	};
+
+	const addItemToList = () => {
+		const newItem: IChecklistItem = {
+			name: itemInput,
+			completed: false,
+			createdDate: Date.now().toString()
+		};
 		setData([...data, newItem]);
-		console.log(data);
+
+		// Deleting the value of the input after user adds an item
+		setItemInput("");
+	};
+
+	const getChecklist = () => {
+		return data.map((elem: IChecklistItem, index: number) => {
+			return (
+				<div className="checklist-item">
+					<div className="checklist-item-name">{elem.completed ? <i>{elem.name}</i> : elem.name}</div>
+					<input
+						id={index.toString()}
+						type="checkbox"
+						checked={elem.completed}
+						onClick={(e) => updateChecklistChecked(e, index)}
+					/>
+				</div>
+			);
+		});
+	};
+
+	const updateChecklistChecked = (e: MouseEvent<HTMLInputElement>, index: number) => {
+		const newItem: IChecklistItem = {
+			name: data[index].name,
+			completed: !data[index].completed,
+			createdDate: data[index].createdDate,
+			completedDate: data[index].completedDate
+		};
+
+		console.log([...data]);
+		const newArray = [...data].splice(index, 0, newItem);
+		console.log(newArray);
+
+		setData(newArray);
 	};
 
 	return (
@@ -40,7 +84,8 @@ export const Checklist = () => {
 			<CiViewList size={80} />
 			Task:
 			{getInputField()}
-			<h2>something</h2>\<p>fds</p>
+			<h2>something</h2>
+			{getChecklist()}
 		</div>
 	);
 };
