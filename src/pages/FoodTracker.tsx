018c0b6b2273335ee22d8b4676084c7321207afd
kienvi <kienvi96@hotmail.com>
2024-06-12 11:00:03 +1000

    Commenting out unusable code Foodtracker.

--- src/pages/FoodTracker.tsx
+++ src/pages/FoodTracker.tsx
@@ -1,57 +1,61 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 export const FoodTracker = () => {
-	const [foodListData, setFoodListData] = useState([]);
+	// const [foodListData, setFoodListData] = useState([]);
 
 	// Food item states
-	const [name, setName] = useState<string>("");
-	const [kj, setKj] = useState<string>("");
-	const [isCommonFood, setIsCommonFood] = useState<boolean>(false);
+	// const [name, setName] = useState<string>("");
+	// const [kj, setKj] = useState<string>("");
+	// const [isCommonFood, setIsCommonFood] = useState<boolean>(false);
 
-	const foodItemForm = () => {
-		return (
-			<div id="food-list-form">
-				<label htmlFor="food-list-name-input">Name</label>
-				<input
-					id="grocery-list-name-input"
-					type="text"
-					name="grocery-list-name-input"
-					onChange={(e) => onNameChange(e)}
-				/>
-				<label htmlFor="grocery-list-quantity-input">Qty</label>
-				<input
-					id="grocery-list-quantity-input"
-					type="text"
-					name="grocery-list-quantity-input"
-					onChange={(e) => onQuantityChange(e)}
-				/>
-				<Dropdown
-					id="grocery-list-measurement-dropdown"
-					label="measurement"
-					options={dropdownOptions}
-					onDropdownChange={(e) => onMeasurementChange(e)}
-				/>
-				{/* <input type="button" value="Add" onClick={() => addGroceryListItem()} /> */}
-			</div>
-		);
-	};
+	// const foodItemForm = () => {
+	// 	return (
+	// 		<div id="food-list-form">
+	// 			<label htmlFor="food-list-name-input">Name</label>
+	// 			<input
+	// 				id="grocery-list-name-input"
+	// 				type="text"
+	// 				name="grocery-list-name-input"
+	// 				onChange={(e) => onNameChange(e)}
+	// 			/>
+	// 			<label htmlFor="grocery-list-quantity-input">Qty</label>
+	// 			<input
+	// 				id="grocery-list-quantity-input"
+	// 				type="text"
+	// 				name="grocery-list-quantity-input"
+	// 				onChange={(e) => onQuantityChange(e)}
+	// 			/>
+	// 			<Dropdown
+	// 				id="grocery-list-measurement-dropdown"
+	// 				label="measurement"
+	// 				options={dropdownOptions}
+	// 				onDropdownChange={(e) => onMeasurementChange(e)}
+	// 			/>
+	// 			{/* <input type="button" value="Add" onClick={() => addGroceryListItem()} /> */}
+	// 		</div>
+	// 	);
+	// };
 
-	const foodList = () => {
-		return foodListData.map((elem) => {
-			return foodListItem(elem);
-		});
-	};
+	// const onNameChange = () => {
 
-	const foodListItem = (elem: any) => {
-		return <div>elem</div>;
-	};
+	// }
+
+	// const foodList = () => {
+	// 	return foodListData.map((elem) => {
+	// 		return foodListItem(elem);
+	// 	});
+	// };
+
+	// const foodListItem = (elem: any) => {
+	// 	return <div>elem</div>;
+	// };
 
 	return (
 		<div id="foodtracker-container">
 			<div></div>
 			<span>hi</span>
-			<div>{foodItemForm()}</div>
-			<div>{foodList()}</div>
+			{/* <div>{foodItemForm()}</div>
+			<div>{foodList()}</div> */}
 		</div>
 	);
 };
