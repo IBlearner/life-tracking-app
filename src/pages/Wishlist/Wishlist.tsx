c2719b950c72b635e5f7e6d0546ea2d76b0c710a
kienvi <kienvi96@hotmail.com>
2024-10-28 16:26:42 +1000

    Adding mock data and mock list to Wishlist.

--- src/pages/Wishlist/Wishlist.tsx
+++ src/pages/Wishlist/Wishlist.tsx
@@ -1,3 +1,24 @@
+import { IWishlistItem } from "../../Interfaces";
+
 export const Wishlist = () => {
-	return "wishlist";
+	const mockWishlist: IWishlistItem[] = [
+		{ id: 0, name: "fdsfds", createdDate: "now" },
+		{ id: 0, name: "dsf", createdDate: "now" },
+		{ id: 0, name: "fdewrwersfds", createdDate: "now" },
+		{ id: 0, name: "fdewr34sfds", createdDate: "now" },
+		{ id: 0, name: "fd43124sfds", createdDate: "now" },
+		{ id: 0, name: "fde23423234Ssfds", createdDate: "now" }
+	];
+	const getList = (items: IWishlistItem[]) => {
+		return items.map((elem) => {
+			return elem.name;
+		});
+	};
+
+	return (
+		<div>
+			<h1>wishlist</h1>
+			<div>{getList(mockWishlist)}</div>
+		</div>
+	);
 };
